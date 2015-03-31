a23243c39295433969108f21491e9ca763800404
Lukáš Jech <klukule@moowdesign.eu>
2015-03-31 22:04:57 +0200

    First day
    Basic gui design (for desktop client), more will come
    No server yet
    
    BUGS:
    Text scalling in champion names
    
    Signed-off-by: Lukáš Jech <klukule@moowdesign.eu>

---
+++ Dekstop client/Dekstop client/CharacterCard.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dekstop_client
+{
+    /// <summary>
+    /// Interaction logic for CharacterCard.xaml
+    /// </summary>
+    public partial class CharacterCard : UserControl
+    {
+        public string championName { get; set; }
+        public int championID { get; set; }
+
+        public CharacterCard(string showName,string imageName,int champID)
+        {
+            InitializeComponent();
+            changeName(showName);
+            changeImage(imageName);
+            this.championID = champID;
+        }
+        public void changeImage(string imageName)
+        {
+            this.champImage.Source = new BitmapImage(new Uri(Properties.Settings.Default.liPre + imageName + Properties.Settings.Default.liSuf, UriKind.Absolute));
+        }
+
+        public void changeName(string name)
+        {
+            championName = name;
+            this.champName.Content = name;
+        }
+    }
+}
---
+++ Dekstop client/Dekstop client/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dekstop_client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            panel.Children.Add(new CharacterCard("Garen", "Garen",1));
+            panel.Children.Add(new CharacterCard("Ahri", "Ahri",2));
+            panel.Children.Add(new CharacterCard("Nunu", "Nunu",3));
+            panel.Children.Add(new CharacterCard("Aatrox", "Aatrox",4));
+            panel.Children.Add(new CharacterCard("Fiddlesticks", "FiddleSticks",5));
+        }
+    }
+}
