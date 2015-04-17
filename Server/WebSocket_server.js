e25c1e6f4ab8c4af93be26ad41bdc4ca27e121dd
Lukáš Jech <klukule@moowdesign.eu>
2015-04-17 16:38:39 +0200

    WebSocket server structure done (next: add hooks to DB) and finish proper client (not just console one)
    Signed-off-by: Lukáš Jech <klukule@moowdesign.eu>

--- Server/WebSocket_server.js
+++ Server/WebSocket_server.js
@@ -1,5 +1,40 @@
-var io = require('socket.io')(process.argv[0]);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+var utils = require('./sharedUtils');
+
+var _PORT = 3000;
+var _UPDATEINTERVAL = 300000;
+
+var connectedClients = 0;
+
+
+//SOCKET HANDLING
 io.on('connection', function(socket){
-  console.log('a user connected');
+  connectedClients++;
+  updateGUI();
+
+  socket.emit('update',"Prvni varka");
+
+  socket.on('disconnect', function(){
+    connectedClients--;
+    updateGUI();
+  });
 });
+http.listen(_PORT, function(){
+  updateGUI();
+  sendUpdate();   //Send first update
+  setInterval(function(){sendUpdate();},_UPDATEINTERVAL);   //Set interval every _UPDATEINTERVAL miliseconds
+
+});
+
+
+function updateGUI(){
+  utils.clearScreen();
+  console.log('connected clients: '+connectedClients);
+}
+
+function sendUpdate(){
+  io.emit("update","dalsi data");
+}
--- SocketIOTestingClient/SocketTestClient/SocketTestClient/Program.cs
+++ SocketIOTestingClient/SocketTestClient/SocketTestClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Quobject.SocketIoClientDotNet.Client;
 
 namespace SocketTestClient
 {
@@ -9,6 +10,21 @@ namespace SocketTestClient
     {
         static void Main(string[] args)
         {
+            var socket = IO.Socket("http://localhost:3000");
+            socket.Connect();
+                socket.On(Socket.EVENT_CONNECT, () =>
+                {
+                    //socket.Emit("chat message", "HELLO ALL");
+
+                });
+
+                socket.On("update", (data) =>
+                {
+                    Console.WriteLine(data);
+                });
+            
+            socket.Disconnect();
+            Console.ReadLine();
         }
     }
 }
